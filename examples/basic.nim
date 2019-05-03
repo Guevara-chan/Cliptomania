1c75a8cbea2ffb39d08e7f36c7e682a60eaef9fe
Victoria Guevara <Guevara-chan@mail.ru>
2019-05-03 12:18:57 +0300

    Add files via upload

--- examples/basic.nim
+++ examples/basic.nim
@@ -8,10 +8,10 @@ import "../src/cliptomania"
 let text_data = "Hallo there." 
 echo "Adding '" & text_data & "' to clipboard..."
 clip.set_text text_data
-if clip.contains_text: echo "Retrieved back: " & clip.get_text()
+if clip.contains_text: echo "Retrieved back: " & clip.get_text
 
 # Droplist coverage.
 let drop_data = @[r"C:\a.txt", r"d:\b.exe"]
 echo "\nAdding '" & $drop_data & "' to clipboard..."
 clip.set_file_drop_list drop_data
-if clip.contains_file_drop_list: echo "Retrieved back: " & $(clip.get_file_drop_list())
+if clip.contains_file_drop_list: echo "Retrieved back: " & $clip.get_file_drop_list
