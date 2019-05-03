149f7ab9f734ec109d9e7004f93bac6c3d478070
Victoria Guevara <Guevara-chan@mail.ru>
2019-05-03 14:21:05 +0300

    Add files via upload

--- examples/basic.nim
+++ examples/basic.nim
@@ -12,10 +12,10 @@ if clip.contains_text: echo "Retrieved back: " & clip.get_text
 
 # Droplist coverage.
 let drop_data = @[r"C:\a.txt", r"d:\b.exe"]
-echo "\nAdding '" & $drop_data & "' to clipboard..."
+echo "--\nAdding '" & $drop_data & "' to clipboard..."
 clip.set_file_drop_list drop_data
 if clip.contains_file_drop_list: echo "Retrieved back: " & $clip.get_file_drop_list
 
 # Final clearance.
 clip.clear
-echo "\nClipboard content erased."
+echo "---\nClipboard content erased."
